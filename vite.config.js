28e71a170b10f07ce36060f7f99f25d5342e9d68
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2025-02-02 18:34:25 +0500

    added tilwaind

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ tailwindcss(),react()],
 })
 
-// export default {
-//   server: {
-//     hmr: {
-//       overlay: false
-//     }
-//   }
-// };
