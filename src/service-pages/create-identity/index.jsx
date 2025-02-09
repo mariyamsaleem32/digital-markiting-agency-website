ce81147e49ed59d4870a470684e50a1410d35018
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2025-02-09 18:55:52 +0500

    added bg

--- src/service-pages/create-identity/index.jsx
+++ src/service-pages/create-identity/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import HeroSection from './heroSection';
 import Offer from './offer';
 import ChooseUs from './chose';
+import "./styles.css";
 
 const Creativity = () => {
     return (
         <>
-            <HeroSection/>
+            <HeroSection className='hero-sec' />
             <Offer/>
             <ChooseUs/>
         </>
